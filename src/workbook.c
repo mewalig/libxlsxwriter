6e4644f2ed2eaec335221107478b6f527dd80fcb
matt w <matt@guarnerix.com>
2020-01-13 17:27:06 -0500

    synced with jmcnamara repo 2849813f3631468d1a9313fd0fd737a8896168c1

--- src/workbook.c
+++ src/workbook.c
@@ -3,7 +3,7 @@
  *
  * Used in conjunction with the libxlsxwriter library.
  *
- * Copyright 2014-2019, John McNamara, jmcnamara@cpan.org. See LICENSE.txt.
+ * Copyright 2014-2020, John McNamara, jmcnamara@cpan.org. See LICENSE.txt.
  *
  */
 
@@ -1852,6 +1852,7 @@ workbook_close(lxw_workbook *self)
     lxw_worksheet *worksheet = NULL;
     lxw_packager *packager = NULL;
     lxw_error error = LXW_NO_ERROR;
+    char codename[LXW_MAX_SHEETNAME_LENGTH] = { 0 };
 
     /* Add a default worksheet if non have been added. */
     if (!self->num_sheets)
@@ -1889,8 +1890,12 @@ workbook_close(lxw_workbook *self)
             else
                 worksheet = sheet->u.worksheet;
 
-            if (!worksheet->vba_codename)
-                worksheet_set_vba_name(worksheet, worksheet->name);
+            if (!worksheet->vba_codename) {
+                lxw_snprintf(codename, LXW_MAX_SHEETNAME_LENGTH, "Sheet%d",
+                             worksheet->index + 1);
+
+                worksheet_set_vba_name(worksheet, codename);
+            }
         }
     }
 
