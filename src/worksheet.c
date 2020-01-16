77f4dfbd50bbc2f5858b0da778be250a517a683c
matt w <matt@guarnerix.com>
2020-01-16 06:13:06 -0500

    add patch for internal hyperlinks not appearing

--- src/worksheet.c
+++ src/worksheet.c
@@ -4909,7 +4909,8 @@ worksheet_write_url_opt(lxw_worksheet *self,
         url_string = lxw_strdup(found_string + 1);
         GOTO_LABEL_ON_MEM_ERROR(url_string, mem_error);
 
-        *found_string = '\0';
+        if(found_string != url_copy) /*** or maybe if(link_type == HYPERLINK_EXTERNAL) ***/
+          *found_string = '\0';
     }
 
     /* Escape the URL. */
